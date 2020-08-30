697b86e40fba6a0d66d130fe48e5a303485b0aa1
Talha <talha@talhasalt.com.tr>
2020-08-30 14:47:50 +0300

    Loading component

---
+++ src/components/Loading.vue
@@ -0,0 +1,11 @@
+<template>
+  <div style="vertical-align: middle; height: 100%;align-content:'center">
+    <img alt="Vue logo" src="./../assets/loading.gif" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Loading",
+};
+</script>
